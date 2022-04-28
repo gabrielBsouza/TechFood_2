ccbdb4eae9907924bedf70970ea0cf4122b438f3
GABRIEL BIFON DE SOUZA <gabriel.bsouza13@senacsp.edu.br>
2022-04-27 21:11:57 -0300

    first commit

---
+++ menu.py
@@ -0,0 +1,42 @@
+import this
+import hamburgao
+import pizza
+import milFolhasFrango
+import milFolhasQueijo
+import bauru
+
+
+
+def Menu ():
+    print('Escolha uma das opções abaixo:\n'+
+          '\n1. Hamburgão R$5,00'+
+          '\n2. Pizza R$5,00'+
+          '\n3. Mil Folhas de Frango R$5,00'+
+          '\n4. Mil Folhas de Queijo R$5,00'+
+          '\n5. Bauru R$5,00'+
+          '\n6. Fechar Programa')
+    this.opcao = int(input())
+
+def operacao():
+    #Mostrar o menu em tela
+    while this.opcao != 6:
+        Menu()
+        #realizar operações
+        if this.opcao == 1:
+            #operacao para 1.
+            hamburgao.coletar()
+        elif this.opcao == 2:
+            #operacao para 2.
+            pizza.coletar()
+        elif this.opcao == 3:
+            #operação para 3.
+            milFolhasFrango.coletar()
+        elif this.opcao == 4:
+            #operacao para 4.
+            milFolhasQueijo.coletar()
+        elif this.opcao == 5:
+            bauru.coletar()
+        elif this.opcao == 6:
+            print('Fechando... agradecemos sua presença aqui!')
+        else:
+            print('Opcao escolhida inválida! Tente novamente com as opções fornecidas.')
