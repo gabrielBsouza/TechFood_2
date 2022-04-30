caa42ace98b00cc6a9d55dee722bee6de37b84d1
murilo.lmesquita <murilolemos2013@gmail.com>
2022-04-29 22:00:35 -0300

    Novo

---
+++ pizza.py
@@ -0,0 +1,51 @@
+import this
+import reserva
+import reservaHorario
+import menu
+this.precoPizza = 5
+this.quantPizza = 10
+this.quantidade = 0
+def Preco ():
+    print('O preco do Hamburgão é: R$' + str(this.precoPizza) + ',00.')
+
+def Quantidade ():
+    print('A quantidade disponível desse lanche é: ' + str(this.quantPizza) + '.')
+
+def Selecao ():
+    print('Digite a quantidade que deseja comprar: ')
+    this.quantidade = int(input())
+    while (this.quantidade < 0) or (this.quantidade > this.quantPizza):
+        if (this.quantidade < 0) or (this.quantidade > this.quantPizza):
+            print('Informe uma quantidade acessível à disponível, por favor!')
+            this.quantidade = int(input())
+    this.quantPizza = this.quantPizza - this.quantidade
+
+def Calculo():
+    return this.quantidade * this.precoPizza
+
+def Compra ():
+    Preco()
+    Quantidade()
+    Selecao()
+    print('O valor total da compra é: '+ str(Calculo()) +',00.')
+
+def Menu():
+    Compra()
+    print('Escolha uma das opções abaixo:\n' +
+            "\n1. Efetuar Reserva " +
+            "\n2. Cancelar e escolher outro produto " +
+            "\n3. Cancelar e fechar o programa ")
+    this.opcao = int(input())
+
+def operacao():
+    Compra()
+    while this.opcao != 3:
+        Menu()
+        if this.opcao == 1:
+            reservaHorario.operacao()
+        elif this.opcao == 2:
+            menu.Menu()
+        elif this.opcao == 3:
+            print('Fechando... agradecemos sua presença aqui!')
+        else:
+            print('Opcao escolhida inválida! Tente novamente com as opções fornecidas.')
---
+++ reservaBauru.py
@@ -0,0 +1,28 @@
+import this
+import bauru
+import reservaHorario
+import menu
+
+def coletar():
+
+    print(str(bauru.Quantidade()))
+
+def Menu():
+    print('Escolha uma das opções abaixo:\n' +
+        "\n1. Efetuar Reserva " +
+        "\n2. Cancelar e escolher outro produto " +
+        "\n3. Cancelar e fechar o programa ")
+    this.opcao = int(input())
+def operacao():
+    coletar()
+    while this.opcao != 3:
+        Menu()
+        # realizar operações
+        if this.opcao == 1:
+            reservaHorario.operacao()
+        elif this.opcao == 2:
+            menu.Menu()
+        elif this.opcao == 3:
+            print('Fechando... agradecemos sua presença aqui!')
+        else:
+            print('Opcao escolhida inválida! Tente novamente com as opções fornecidas.')
